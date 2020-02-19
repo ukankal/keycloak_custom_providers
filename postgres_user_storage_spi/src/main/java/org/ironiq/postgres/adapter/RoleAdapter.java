fdc92dd5819de5312fa18098c61df923b284f247
ukankal <uday@datanowsolutions.com>
2020-02-19 19:41:06 +0530

    Uday | handled bugs related to groups and roles methods

--- postgres_user_storage_spi/src/main/java/org/ironiq/postgres/adapter/RoleAdapter.java
+++ postgres_user_storage_spi/src/main/java/org/ironiq/postgres/adapter/RoleAdapter.java
@@ -7,22 +7,27 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RoleModel;
 import org.keycloak.models.RoleContainerModel;
-import org.keycloak.storage.StorageId;
 
 import java.util.Set;
 import java.util.List;
 import java.util.Map;
 import java.util.Collection;
+import java.util.Collections;
 
 public class RoleAdapter implements RoleModel {
 
   private final Role role;
   private final RoleDaoImpl roleDaoImpl;
+  protected KeycloakSession session;
+  protected ComponentModel model;
+  protected RealmModel realm;
 
-  public RoleAdapter(KeycloakSession session, RealmModel realm, ComponentModel model, Role role,
-      RoleDaoImpl roleDaoImpl) {
+  public RoleAdapter(KeycloakSession session, RealmModel realm, ComponentModel model, Role role) {
     this.role = role;
-    this.roleDaoImpl = roleDaoImpl;
+    this.roleDaoImpl = new RoleDaoImpl();
+    this.session = session;
+    this.realm = realm;
+    this.model = model;
   }
 
   public String getName() {
@@ -60,19 +65,25 @@ public class RoleAdapter implements RoleModel {
   }
 
   public Set<RoleModel> getComposites() {
-    return null;
+    return Collections.emptySet();
   }
 
   public boolean isClientRole() {
-    return false;
+    return roleDaoImpl.isClientRole(role.getId());
   }
 
   public String getContainerId() {
-    return null;
+    return getContainer().getId();
   }
 
   public RoleContainerModel getContainer() {
-    return null;
+    if (isClientRole()) {
+      String client_name = roleDaoImpl.getClientName(role.getId());
+      if (client_name != null) {
+        return realm.getClientByClientId(client_name);
+      }
+    }
+    return realm;
   }
 
   public boolean hasRole(RoleModel role) {
@@ -96,10 +107,10 @@ public class RoleAdapter implements RoleModel {
   }
 
   public List<String> getAttribute(String name) {
-    return null;
+    return Collections.emptyList();
   }
 
   public Map<String, List<String>> getAttributes() {
-    return null;
+    return Collections.emptyMap();
   }
 }
